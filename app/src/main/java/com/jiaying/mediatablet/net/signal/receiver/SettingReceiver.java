9ed22fdbf11f7e7f9fceabdff5ab2cdb7e4c3142
hipilee <hipilee@gmail.com>
2016-09-18 17:33:48 +0800

    1-通过命令模式，降低了MainActivity的复杂度

---
+++ app/src/main/java/com/jiaying/mediatablet/net/signal/receiver/SettingReceiver.java
@@ -0,0 +1,39 @@
+package com.jiaying.mediatablet.net.signal.receiver;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jiaying.mediatablet.R;
+import com.jiaying.mediatablet.activity.MainActivity;
+import com.jiaying.mediatablet.fragment.ServerSettingFragment;
+
+/**
+ * Created by hipil on 2016/9/14.
+ */
+public class SettingReceiver extends Receiver {
+    private MainActivity mainActivity;
+
+    public SettingReceiver(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void work() {
+        //调整界面组成
+        this.mainActivity.uiComponent(false, true, false, false);
+
+        //界面切换
+        this.mainActivity.switchFragment(R.id.fragment_container, new ServerSettingFragment());
+
+        //调整界面文字
+        TextView tv_title = this.mainActivity.getTitleTV();
+        tv_title.setText(R.string.fragment_wait_plasm_title);
+
+        //设置logo的相关事件
+        ImageView ivLogoAndBack = this.mainActivity.getIvLogoAndBack();
+        ivLogoAndBack.setEnabled(false);
+        ivLogoAndBack.setImageResource(R.mipmap.ic_launcher);
+
+        //启动相关动作
+    }
+}
